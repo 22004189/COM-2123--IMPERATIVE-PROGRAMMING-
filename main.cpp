cb6a0b923b4749ab8f7ac1177bbffb286d6de438
22004189 <143625338+22004189@users.noreply.github.com>
2024-03-16 02:06:42 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    double merchandise,storeRent,employeesSalary,electricCost,netProfit,finalAmount
-    ;
-    double percent = 0.15;
+    double distanceD,heightH,ht,angle;
+    //ht is the height of the tree;
 
-    cout<<"Enter the total cost of merchandise: ";
-    cin>>merchandise;
-    cout<< "Enter the value of store rent:" ;
-    cin>>storeRent;
-    cout<<"Enter the total amount of salary of the employees:" ;
-    cin>>employeesSalary;
-    cout<<"Enter the total amount of electricity  cost:";
-    cin>> electricCost;
+    const double PI= 3.142;
+    int halfCycle=180;
 
-    netProfit= merchandise*0.15 + employeesSalary +electricCost;
+    cout<<"Enter distance D in feet:";
+    cin>>distanceD;
 
-    cout<<"The total net profit is:"<<netProfit<<endl;
+    cout<<"Enter height H in inches:";
+    cin>>heightH;
+
+    cout<<"Enter the angle of theta in degrees:";
+    cin>>angle;
+
+    ht=heightH+distanceD*tan(angle*PI/halfCycle);
+    cout<<"The height of the tree:"<<ht<< "feet";
+
+
+
 
     return 0;
 }
